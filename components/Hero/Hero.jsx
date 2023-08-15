20e67a68dea6ce94e1500c75d9fd021741fe90be
Arjun Saini <arjuns1026@gmail.com>
2023-08-14 21:34:51 -0400

    File Structure

---
+++ components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '@/styles/globals.css';
+
+const Hero = () => {
+	return (
+		<section className="wrapper">
+			<h1>Hello, I'm Arjun.</h1>
+			<h2>A 19 year old Software Engineer.</h2>
+		</section>
+	);
+};
+
+export default Hero;
