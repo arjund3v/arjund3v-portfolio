3022e2ec163142aece477ab3f4b04b4fc21aeee6
Arjun Saini <arjuns1026@gmail.com>
2023-08-23 15:43:30 -0400

    Started about section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import NavBar from '@/components/NavBar/NavBar';
 import Hero from '@/components/Hero/Hero';
+import About from '@/components/About/About';
 
 export default function Home() {
 	return (
 		<main>
 			<NavBar />
 			<Hero />
+			<About />
 		</main>
 	);
 }
---
+++ components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '@/styles/globals.css';
+
+const About = () => {
+	return (
+		<section
+			id="about"
+			className="h-full"
+		></section>
+	);
+};
+
+export default About;
