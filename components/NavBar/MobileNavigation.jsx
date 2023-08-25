7a074c486153eb29358c2f056249d8df97a61768
Arjun Saini <arjuns1026@gmail.com>
2023-08-25 00:15:21 -0400

    Split up Navbar
    + Re doing navbar
    + Split into multiple components to make code easier to work with

---
+++ components/NavBar/MobileNavigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavLinks from './Navlinks';
+
+const MobileNavigation = () => {
+	return (
+		<nav className="MobileNavigation">
+			<NavLinks />
+		</nav>
+	);
+};
+
+export default MobileNavigation;
---
+++ components/NavBar/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavLinks from './Navlinks';
+
+const Navigation = () => {
+	return (
+		<nav>
+			<NavLinks />
+		</nav>
+	);
+};
+
+export default Navigation;
