a63a9f0ed3501175a88f5a8a2023b40a04386f1d
Arjun Saini <arjuns1026@gmail.com>
2023-09-04 10:01:41 -0400

    Added basic animation + spacing
    + Added basic animation to the cards container
    
    + Will probably redo the cards to add a nicer animation to each

--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -1,4 +1,6 @@
+'use client';
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { FaReact, FaPython, FaAws, FaSpotify, FaDatabase, FaNodeJs } from 'react-icons/fa';
 import { SiNextdotjs, SiTailwindcss, SiC, SiExpress } from 'react-icons/si';
@@ -9,24 +11,47 @@ import '@/styles/globals.css';
 import './Projects.css';
 
 const Projects = () => {
+	const fadeInAnimationVariants = {
+		initial: {
+			opacity: 0,
+			y: 100,
+		},
+		animate: {
+			opacity: 1,
+			y: 0,
+			transition: {
+				type: 'spring',
+				damping: 15,
+				stiffness: 180,
+			},
+		},
+	};
 	return (
 		<section
-			className="projects bg-gray-100 flex flex-col gap-24"
+			className="projects bg-gray-100 flex flex-col justify-center items-center align-middle text-center gap-24"
 			id="projects"
 		>
-			<div class="flex flex-col justify-center align-middle items-center text-center gap-10">
-				<div>
-					<h3 className="noto-extra text-darkest text-lg">PROJECTS</h3>
-					<h2 className="text-5xl noto-extra">
-						Many More Can Be Found On My{' '}
-						<a
-							className="text-darkest"
-							href="https://github.com/arjund3v/"
-						>
-							GitHub!
-						</a>
-					</h2>
-				</div>
+			<div>
+				<h3 className="noto-extra text-darkest text-lg my-2">PROJECTS</h3>
+				<h2 className="text-5xl noto-extra">
+					Many More Can Be Found On My{' '}
+					<a
+						className="text-darkest underline"
+						href="https://github.com/arjund3v/"
+					>
+						GitHub!
+					</a>
+				</h2>
+			</div>
+			<motion.div
+				variants={fadeInAnimationVariants}
+				initial="initial"
+				whileInView="animate"
+				viewport={{
+					once: true,
+				}}
+				class="flex flex-col justify-center align-middle items-center text-center gap-14"
+			>
 				<div className="flex flex-col flex-wrap gap-5 justify-evenly align-baseline items-end md:flex-row md:w-4/5">
 					<div className="card">
 						<p className="card-title">Study.Me</p>
@@ -103,6 +128,48 @@ const Projects = () => {
 							<a
 								className="live-demo noto"
 								href="https://github.com/arjund3v/Spotify-ETL-Pipeline"
+							>
+								Live Demo
+								<span>
+									<HiMiniArrowTopRightOnSquare
+										className="inline mx-1"
+										size={20}
+									/>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div className="card">
+						<p className="card-title">Store API</p>
+						<p className="small-desc">An API created using ExpressJS + NodeJS, which takes a real world approach and focuses on filtering methods, query parameters, etc.</p>
+						<div className="icons-container">
+							<SiExpress size={35} />
+							<FaNodeJs size={35} />
+						</div>
+						<div className="go-corner">
+							<a
+								href="https://github.com/arjund3v/store-api"
+								className="go-arrow"
+							>
+								→
+							</a>
+						</div>
+						<div className="buttons-container">
+							<a
+								className="github-button noto"
+								href="https://github.com/arjund3v/store-api"
+							>
+								GitHub
+								<span>
+									<LuGithub
+										className="inline mx-1"
+										size={20}
+									/>
+								</span>
+							</a>
+							<a
+								className="live-demo noto"
+								href="https://github.com/arjund3v/store-api"
 							>
 								Live Demo
 								<span>
@@ -198,50 +265,8 @@ const Projects = () => {
 							</a>
 						</div>
 					</div>
-					<div className="card">
-						<p className="card-title">Store API</p>
-						<p className="small-desc">An API created using ExpressJS + NodeJS, which takes a real world approach and focuses on filtering methods, query parameters, etc.</p>
-						<div className="icons-container">
-							<SiExpress size={35} />
-							<FaNodeJs size={35} />
-						</div>
-						<div className="go-corner">
-							<a
-								href="https://github.com/arjund3v/C-Programs-Archive"
-								className="go-arrow"
-							>
-								→
-							</a>
-						</div>
-						<div className="buttons-container">
-							<a
-								className="github-button noto"
-								href="https://github.com/arjund3v/C-Programs-Archive"
-							>
-								GitHub
-								<span>
-									<LuGithub
-										className="inline mx-1"
-										size={20}
-									/>
-								</span>
-							</a>
-							<a
-								className="live-demo noto"
-								href="https://github.com/arjund3v/C-Programs-Archive"
-							>
-								Live Demo
-								<span>
-									<HiMiniArrowTopRightOnSquare
-										className="inline mx-1"
-										size={20}
-									/>
-								</span>
-							</a>
-						</div>
-					</div>
 				</div>
-			</div>
+			</motion.div>
 		</section>
 	);
 };
